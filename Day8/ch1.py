dca38586a6798410e5ba6abc0035dc214f113dc2
Swafox <25106596+Swafox@users.noreply.github.com>
2020-11-14 11:58:57 +0200

    Challenge 1

---
+++ Day8/ch1.py
@@ -0,0 +1,18 @@
+# Day 8
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print("Caesar Cipher")
+text = input("Type your message: ").lower()
+shift = int(input("Type the shift number: "))
+
+#Don't change the code above 👆
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    cipher_text += new_letter
+  print(f"The encoded text is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
