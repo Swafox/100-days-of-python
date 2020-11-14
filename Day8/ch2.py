0ebf3f62715d44c75e001f4d4eef9e7a084e761f
Swafox <25106596+Swafox@users.noreply.github.com>
2020-11-14 12:10:34 +0200

    Challenge 2

---
+++ Day8/ch2.py
@@ -0,0 +1,15 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print("Decrypt Caesar Cipher")
+text = input("Type your message: ").lower()
+shift = int(input("Type the shift number: "))
+
+def decrypt(cipher_text, shift_amount):
+  plain_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    plain_text += alphabet[new_position]
+  print(f"The decoded text is {plain_text}")
+
+decrypt(cipher_text=text, shift_amount=shift)
